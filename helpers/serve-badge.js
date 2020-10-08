5e5d3ac92cad15153a98ec0cae516a9895701822
Tuan Anh Tran <me@tuananh.org>
2020-10-08 15:05:05 +0700

    Update serve-badge.js

--- helpers/serve-badge.js
+++ helpers/serve-badge.js
@@ -1,11 +1,15 @@
 import { badgen } from 'badgen'
+import config from '../config'
 
 const serveBadge = async (badgenOpts) => {
     const svg = badgen(badgenOpts)
 
     return new Response(svg, {
         status: 200,
-        headers: { 'Content-Type': 'image/svg+xml' },
+        headers: { 
+            'Content-Type': 'image/svg+xml',
+            'Cache-Control': `max-age=${ms(config.defaultCacheDurationSecond) / 1000}`,
+        },
     })
 }
 
