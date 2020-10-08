92070bc8f7ae2c04e8af0ae9ce89b766c4a6992e
Tuan Anh Tran <anh.tt@teko.vn>
2020-10-08 15:08:53 +0700

    forgot to import ms
    Signed-off-by: Tuan Anh Tran <anh.tt@teko.vn>

--- helpers/serve-badge.js
+++ helpers/serve-badge.js
@@ -1,16 +1,19 @@
-import { badgen } from 'badgen'
-import config from '../config'
+import { badgen } from "badgen";
+import ms from "ms";
+import config from "../config";
 
 const serveBadge = async (badgenOpts) => {
-    const svg = badgen(badgenOpts)
+  const svg = badgen(badgenOpts);
 
-    return new Response(svg, {
-        status: 200,
-        headers: { 
-            'Content-Type': 'image/svg+xml',
-            'Cache-Control': `max-age=${ms(config.defaultCacheDurationSecond) / 1000}`,
-        },
-    })
-}
+  return new Response(svg, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/svg+xml",
+      "Cache-Control": `max-age=${
+        ms(config.defaultCacheDurationSecond) / 1000
+      }`,
+    },
+  });
+};
 
-export default serveBadge
+export default serveBadge;
