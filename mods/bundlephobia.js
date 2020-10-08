9f22dc2d0604332fddf6f4298b0566b64c782fee
Tuan Anh Tran <anh.tt@teko.vn>
2020-10-08 14:12:05 +0700

    fix bundlephobia
    Signed-off-by: Tuan Anh Tran <anh.tt@teko.vn>

--- mods/bundlephobia.js
+++ mods/bundlephobia.js
@@ -1,41 +1,69 @@
-import serveBadge from '../helpers/serve-badge'
+import byteSize from "byte-size";
+import serveBadge from "../helpers/serve-badge";
 
 async function handlePackagephobia(request) {
-    const { pathname } = new URL(request.url)
-    const parts = pathname.split('/')
-    console.log(parts)
-    if (parts.length > 3) {
-        const pkgName = parts[3]
-        const resp = await fetch(
-            `https://bundlephobia.com/api/size?package=${pkgName}@latest&record=true`
-        )
-        const json = await resp.json()
-        console.log(json)
-        // TODO(anh): cache response
-        // TODO pretty print size
-        if (json.gzip) {
-            const type = parts[2]
-            switch (type) {
-                case 'min':
-                    return serveBadge({ label: type, status: (json.size / 1024).toFixed(0) + ' KB' })
-                case 'minzip':
-                    return serveBadge({ label: type, status: (json.gzip / 1024).toFixed(0) + ' KB' })
-                case 'dependency-count':
-                    return serveBadge({ label: type, status: json.dependencyCount })
-                case 'tree-shaking':
-                    return serveBadge({ label: type, status: json.dependencyCount })
-                default:
-                    return new Response('bad request')
-            }
-        } else {
-            return new Response(
-                'bad bad response from npm, got ' +
-                    JSON.stringify(json, null, 2)
-            )
-        }
+  const { pathname } = new URL(request.url);
+  const parts = pathname.split("/");
+  console.log(parts);
+  if (parts.length > 3) {
+    const topic = parts[2];
+    const pkgName = parts[3];
+    const resp = await fetch(
+      `https://bundlephobia.com/api/size?package=${pkgName}@latest&record=true`
+    );
+    if (resp.status === 200) {
+      const {
+        size,
+        gzip,
+        dependencyCount,
+        hasJSModule,
+        hasJSNext,
+      } = await resp.json();
+      switch (topic) {
+        case "min":
+          return serveBadge({
+            label: topic,
+            status: byteSize(size, { units: "iec" })
+              .toString()
+              .replace(/iB\b/, "B"),
+          });
+        case "minzip":
+          return serveBadge({
+            label: topic,
+            status: byteSize(gzip, { units: "iec" })
+              .toString()
+              .replace(/iB\b/, "B"),
+            color: "blue",
+          });
+        case "dependency-count":
+          return serveBadge({
+            label: topic,
+            status: dependencyCount,
+            color: "blue",
+          });
+        case "tree-shaking":
+          const isTreeShakeable = hasJSModule || hasJSNext;
+          return serveBadge({
+            label: topic,
+            status: isTreeShakeable ? "supported" : "not supported",
+            color: isTreeShakeable ? "green" : "red",
+          });
+        default:
+          return serveBadge({
+            subject: "bundlephobia",
+            status: "unknown",
+            color: "grey",
+          });
+      }
     }
+    return serveBadge({ label: topic, status: json.dependencyCount });
+  }
 
-    return new Response('bad bad request')
+  return serveBadge({
+    subject: "bundlephobia",
+    status: "unknown",
+    color: "grey",
+  });
 }
 
-export default handlePackagephobia
+export default handlePackagephobia;
