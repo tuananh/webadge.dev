416f654536d4fc35c8414e2b730dfe90ad00c149
Tuan Anh Tran <me@tuananh.org>
2020-10-09 22:37:20 +0700

    update cachedExecute to save ttl as well
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- config/index.js
+++ config/index.js
@@ -2,7 +2,6 @@ import ms from "ms";
 
 export default {
   defaultCacheDurationSecond: ms("10m") / 1000,
-  serveStaleData: true,
   githubGraphqlUrl: "https://api.github.com/graphql",
   githubApiUrl: "https://api.github.com/",
 };
--- helpers/cached-execute.js
+++ helpers/cached-execute.js
@@ -1,25 +1,31 @@
-import config from '../config'
+import config from "../config";
 
 async function cachedExecute({ key, loadFn, json }) {
-    let value = await badgeKV.get(key)
-    if (value === null) {
-        try {
-            value = await loadFn()
-            if (value) {
-                badgeKV.put(key, JSON.stringify(value), {
-                    expirationTtl: config.defaultCacheDurationSecond
-                })
-            }
-            return value
-        } catch (err) {
-            console.log('error in ' + loadFn.name)
-            throw err
-        }
+  const ttlKey = `ttl:${key}`;
+  let [value, ttl] = await Promise.all([badgeKV.get(key), badgeKV.get(ttlKey)]);
+  const currentTimestamp = new Date().getTime();
+  const shouldReload = value === null || ttl === null || ttl < currentTimestamp;
+  if (shouldReload) {
+    try {
+      value = await loadFn();
+      if (value) {
+        // we don't expire key here. Instead, we use another key for storing ttl.
+        // when the data is expired, we will serve the staled data and refresh in the background
+        badgeKV.put(key, JSON.stringify(value));
+        badgeKV.put(
+          ttlKey,
+          currentTimestamp + config.defaultCacheDurationSecond * 1000
+        );
+      }
+      return value;
+    } catch (err) {
+      console.log("error in " + loadFn.name);
+      throw err;
     }
-    if (json === true) {
-        return JSON.parse(value)
-    } else return value
-    
+  }
+  if (json === true) {
+    return JSON.parse(value);
+  } else return value;
 }
 
-export default cachedExecute
+export default cachedExecute;
