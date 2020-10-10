faae9d1dabb1d7337449ac35968dc5c9e6b19dca
Tuan Anh Tran <me@tuananh.org>
2020-10-10 07:05:06 +0700

    ...
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- helpers/cached-execute.js
+++ helpers/cached-execute.js
@@ -16,33 +16,39 @@ async function cachedExecute({ key, loadFn, json }) {
   let [value, ttl] = await Promise.all([badgeKV.get(key), badgeKV.get(ttlKey)]);
   const currentTimestamp = new Date().getTime();
   const shouldReload = value === null || ttl === null || ttl < currentTimestamp;
+
+  async function reload() {
+    const newVal = await loadFn();
+    setTimeout(() => {
+      if (newVal) {
+        // we don't expire key here. Instead, we use another key for storing ttl.
+        // when the data is expired, we will serve the staled data and refresh in the background
+        badgeKV.put(key, JSON.stringify(newVal), {
+          expirationTtl: config.staledDataTtl,
+        });
+        badgeKV.put(
+          ttlKey,
+          currentTimestamp + config.defaultCacheDurationSecond * 1000,
+          {
+            expirationTtl: config.staledDataTtl,
+          }
+        );
+      }
+    }, 0);
+
+    return newVal;
+  }
+
   if (shouldReload) {
     try {
-      // serving the staled data if it's not yet removed from workers kv
       if (value) {
-        console.log("value", value);
+        // cache is there but staled
+        reload();
         return parseJsonOrNot(value, json);
       } else {
-        // reload
-        value = await loadFn();
-        console.log("value", value);
-        setTimeout(() => {
-          if (value) {
-            // we don't expire key here. Instead, we use another key for storing ttl.
-            // when the data is expired, we will serve the staled data and refresh in the background
-            badgeKV.put(key, JSON.stringify(value), {
-              expirationTtl: config.staledDataTtl,
-            });
-            badgeKV.put(
-              ttlKey,
-              currentTimestamp + config.defaultCacheDurationSecond * 1000,
-              {
-                expirationTtl: config.staledDataTtl,
-              }
-            );
-          }
-        }, 0);
-        return parseJsonOrNot(value, json);
+        // cache not found
+        const newVal = await reload();
+        return parseJsonOrNot(newVal, json);
       }
     } catch (err) {
       console.log("error in " + loadFn.name);
