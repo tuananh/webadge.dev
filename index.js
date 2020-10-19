8232f04f195d086fbfc2865f372a58636681b584
Anh Thang Bui <buianhthang89@gmail.com>
2020-10-19 23:50:37 +0700

    fix: router handler

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 import config from "./config";
 import Router from "cloudworker-router";
-import handleStaticBadge from "./mods/static";
-import handleNpm from "./mods/npm";
-import handleBundlephobia from "./mods/bundlephobia";
-import handlePackagephobia from "./mods/packagephobia";
-import handleTravisCI from "./mods/travis";
-import handleAppveyor from "./mods/appveyor";
-import handleGitHub from "./mods/github";
+import * as handlers from "./mods";
 
 const router = new Router();
 
@@ -21,37 +15,23 @@ async function routeHandler(ctx, handler) {
   ctx.status = 200;
 }
 
-// TODO(anh): optional param is not supported yet hence we have to duplicate route
-router.get("/badge/:label/:status", (ctx) =>
-  routeHandler(ctx, handleStaticBadge)
-);
-router.get("/badge/:label/:status/:color", (ctx) =>
-  routeHandler(ctx, handleStaticBadge)
-);
+const handlerMap = {
+  "/badge/:label/:status": handlers.badge,
+  "/badge/:label/:status/:color": handlers.badge,
+  "/npm/:topic/:pkgName": handlers.npm,
+  "/github/:topic/:owner/:repo": handlers.github,
+  "/bundlephobia/:topic/:pkgName": handlers.bundlephobia,
+  "/packagephobia/:topic/:pkgName": handlers.packagephobia,
+  "/packagephobia/:topic/:scope/:pkgName": handlers.packagephobia,
+  "/travis/:user/:repo/:branch": handlers.travis,
+  "/travis/:user/:repo": handlers.travis,
+  "/appveyor/:account/:project/:branch": handlers.appveyor,
+  "/appveyor/:account/:project": handlers.appveyor,
+};
 
-router.get("/npm/:topic/:pkgName", (ctx) => routeHandler(ctx, handleNpm));
-router.get("/github/:topic/:owner/:repo", (ctx) =>
-  routeHandler(ctx, handleGitHub)
-);
-router.get("/bundlephobia/:topic/:pkgName", (ctx) =>
-  routeHandler(ctx, handleBundlephobia)
-);
-router.get("/packagephobia/:topic/:pkgName", (ctx) =>
-  routeHandler(ctx, handlePackagephobia)
-);
-router.get("/packagephobia/:topic/:scope/:pkgName", (ctx) =>
-  routeHandler(ctx, handlePackagephobia)
-);
-router.get("/travis/:user/:repo/:branch", (ctx) =>
-  routeHandler(ctx, handleTravisCI)
-);
-router.get("/travis/:user/:repo", (ctx) => routeHandler(ctx, handleTravisCI));
-router.get("/appveyor/:account/:project/:branch", (ctx) =>
-  routeHandler(ctx, handleAppveyor)
-);
-router.get("/appveyor/:account/:project", (ctx) =>
-  routeHandler(ctx, handleAppveyor)
-);
+Object.entries(handlerMap).map(([path, handler]) => {
+  router.get(path, (ctx) => routeHandler(ctx, handler));
+});
 
 addEventListener("fetch", (event) => {
   event.respondWith(router.resolve(event));
---
+++ mods/index.js
@@ -0,0 +1,9 @@
+import appveyor from "./appveyor";
+import badge from "./static";
+import bundlephobia from "./bundlephobia";
+import github from "./github";
+import npm from "./npm";
+import packagephobia from "./packagephobia";
+import travis from "./travis";
+
+export { appveyor, badge, bundlephobia, github, npm, packagephobia, travis };
