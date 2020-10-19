9b430551cee6c3b19be5c8e59e473ebe6d4b1446
Anh Thang Bui <buianhthang89@gmail.com>
2020-10-19 17:16:24 +0700

    `list` supported

--- helpers/badge.js
+++ helpers/badge.js
@@ -3,6 +3,9 @@ import { badgen } from "badgen";
 async function serveBadge(badge, options) {
   Object.keys(options).forEach((key) => {
     switch (key) {
+      case "list":
+        badge.status = badge.status.replaceAll(",", ` ${options.list || "|"} `);
+        break;
       case "label":
         options.label = decodeURIComponent(options.label);
       default:
